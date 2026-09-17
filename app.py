cea6d2c2bfd6bc815a7a6b4c3399b4b21bc83731
aledan413 <aledan413@gmail.com>
2026-09-17 03:26:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ from firebase_admin import credentials, firestore
 # FIREBASE
 # ============================================================
 
-cred = credentials.Certificate("firebase_credentials.json")
-
 if not firebase_admin._apps:
+    cred = credentials.Certificate(st.secrets["firebase"])
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
